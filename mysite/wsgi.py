fe88d424d5ba9c52de3e06b914a0d907c6e931e8
HZXLZ <hzxlz@example.com>
2025-08-10 00:33:30 +0800

    强制应用Dummy设置：添加django.setup()

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,12 +8,14 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import django
 from django.core.wsgi import get_wsgi_application
 
 # 强制设置Dummy模式设置（用于Vercel部署测试）
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings_dummy'
-
-# 设置Dummy数据库URL
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings_dummy')
 os.environ['DATABASE_URL'] = 'dummy://localhost:5432/dummy'
+
+# 强制应用设置
+django.setup()
 
 app = get_wsgi_application()
